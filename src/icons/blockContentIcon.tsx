1be8db29556ded870727f356b1e6f993634e4da5
jorngeorg <968695+jorngeorg@users.noreply.github.com>
2022-11-25 16:03:11 +0100

    fix: correct viewbox and currentColor

--- src/icons/blockContentIcon.tsx
+++ src/icons/blockContentIcon.tsx
@@ -14,7 +14,7 @@ export const BlockContentIcon = forwardRef(function BlockContentIcon(
       data-sanity-icon="block-content"
       width="1em"
       height="1em"
-      viewBox="0 0 26 25"
+      viewBox="0 0 25 25"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       ref={ref}
@@ -23,30 +23,30 @@ export const BlockContentIcon = forwardRef(function BlockContentIcon(
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M21.0126 7.59999L11.0126 7.59999V6.39999L21.0126 6.39999V7.59999Z"
-        fill="#101112"
+        d="M21 7.59999L11 7.59999V6.39999L21 6.39999V7.59999Z"
+        fill="currentColor"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M21.0126 11.2667L12.4959 11.2667V10.0667L21.0126 10.0667V11.2667Z"
-        fill="#101112"
+        d="M21 11.2667L12.4833 11.2667V10.0667L21 10.0667V11.2667Z"
+        fill="currentColor"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M21.0126 14.9334H13.938V13.7334L21.0126 13.7334V14.9334Z"
-        fill="#101112"
+        d="M21 14.9334H13.9254V13.7334L21 13.7334V14.9334Z"
+        fill="currentColor"
       />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M21.0126 18.6002H4.01263V17.4002H21.0126V18.6002Z"
-        fill="#101112"
+        d="M21 18.6002H4V17.4002H21V18.6002Z"
+        fill="currentColor"
       />
       <path
-        d="M9.87701 15.6H11.2126L8.28886 7.59999H6.93641L4.01263 15.6H5.30336L6.04973 13.4766H9.13625L9.87701 15.6ZM7.5481 9.05252H7.6435L8.81638 12.4344H6.36961L7.5481 9.05252Z"
-        fill="#101112"
+        d="M9.86438 15.6H11.2L8.27623 7.59999H6.92377L4 15.6H5.29072L6.0371 13.4766H9.12362L9.86438 15.6ZM7.53546 9.05252H7.63086L8.80374 12.4344H6.35698L7.53546 9.05252Z"
+        fill="currentColor"
       />
     </svg>
   )
