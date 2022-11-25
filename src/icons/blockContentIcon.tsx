921ff78704a4437e15fcae302e5eead5a0c9c571
jorngeorg <968695+jorngeorg@users.noreply.github.com>
2022-11-25 13:42:56 +0100

    feat: add number, string and block content icon

---
+++ src/icons/blockContentIcon.tsx
@@ -0,0 +1,53 @@
+/* THIS FILE IS AUTO-GENERATED – DO NOT EDIT */
+
+import {forwardRef} from 'react'
+
+/**
+ * @public
+ */
+export const BlockContentIcon = forwardRef(function BlockContentIcon(
+  props: React.SVGProps<SVGSVGElement>,
+  ref: React.Ref<SVGSVGElement>
+) {
+  return (
+    <svg
+      data-sanity-icon="block-content"
+      width="1em"
+      height="1em"
+      viewBox="0 0 26 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      ref={ref}
+      {...props}
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M21.0126 7.59999L11.0126 7.59999V6.39999L21.0126 6.39999V7.59999Z"
+        fill="#101112"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M21.0126 11.2667L12.4959 11.2667V10.0667L21.0126 10.0667V11.2667Z"
+        fill="#101112"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M21.0126 14.9334H13.938V13.7334L21.0126 13.7334V14.9334Z"
+        fill="#101112"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M21.0126 18.6002H4.01263V17.4002H21.0126V18.6002Z"
+        fill="#101112"
+      />
+      <path
+        d="M9.87701 15.6H11.2126L8.28886 7.59999H6.93641L4.01263 15.6H5.30336L6.04973 13.4766H9.13625L9.87701 15.6ZM7.5481 9.05252H7.6435L8.81638 12.4344H6.36961L7.5481 9.05252Z"
+        fill="#101112"
+      />
+    </svg>
+  )
+})
