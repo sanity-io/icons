09f1118e51562870ec89364baee26fd111656b04
Cody Olsen <stipsan@gmail.com>
2024-06-07 12:56:11 +0200

    feat!: initial test with react compiler

--- package.config.ts
+++ package.config.ts
@@ -1,6 +1,9 @@
 import {defineConfig} from '@sanity/pkg-utils'
 
 export default defineConfig({
+  babel: {
+    reactCompiler: true,
+  },
   extract: {
     rules: {
       'ae-internal-missing-underscore': 'off',
