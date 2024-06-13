51461256d5b8dfd35368e1f143b116c915667ddc
Cody Olsen <stipsan@gmail.com>
2024-06-13 22:23:43 +0200

    feat: add `react-compiler` condition

--- package.config.ts
+++ package.config.ts
@@ -7,4 +7,23 @@ export default defineConfig({
     },
   },
   tsconfig: 'tsconfig.dist.json',
+  reactCompilerOptions: {
+    panicThreshold: 'ALL_ERRORS',
+    logger: {
+      logEvent(filename, event) {
+        if (event.kind === 'CompileError') {
+          console.group(`[${filename}] ${event.kind}`)
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          const {reason, description, severity, loc, suggestions} = event.detail as any
+
+          console.error(`[${severity}] ${reason}`)
+          console.log(`${filename}:${loc.start.line}:${loc.start.column} ${description}`)
+          for (const suggestion of suggestions) {
+            console.log(suggestion.description)
+          }
+          console.groupEnd()
+        }
+      },
+    },
+  },
 })
